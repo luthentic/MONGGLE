4f7a415802c003b979d07a6baa1d264dc92d6728
DESKTOP-HHHASMJ\dltpd <dltpdnd12345@naver.com>
2021-08-21 00:22:18 -0700

    test2

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,7 @@
 console.log("test");
 console.log("test");
 console.log("test");
-console.log("test");
-console.log("test");
-console.log("test");
+let b = console.log("test2");
 
 let a = prompt("answer","LEE");
 console.log("lee");
